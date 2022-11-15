41dbf818ea0f7171d39e25b326f7b84ef86d8740
conghuy0908 <102871202+conghuy0908@users.noreply.github.com>
2022-11-15 20:07:05 +0700

    Fixx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import axios from "axios";
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todolist: [],
+      todo:'',
+      loading: false
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then(res => {
+        this.setState({ todolist: res.data });
+      })
+      .catch(err => {
+        throw err;
+      });
+  }
+  handleChange = (event) => {
+    this.settodo({
+      ...this.todo,
+      [event.target.name]: event.target.value
+    });
+  }
+  handleSubmit() {
+        axios
+          .post("	https://jsonplaceholder.typicode.com/todos", this.state.todolist)
+          .then(res => {
+            alert("successfully!!!");
+          })
+          .catch(err => {
+            throw err;
+          });
 }
 
-export default App;
+  render() {
+    const { todolist } = this.state;
+    return (
+      <div className="container">
+        <h1>TodoList</h1>
+        <input type='text' value={this.todo} onChange={this.handleChange}/>
+        <button type="submit" onClick={this.handleSubmit}>Submit</button>
+        <ul>
+        {todolist.map(todo => (<li value={todo.id}>Todo {todo.id}</li>))};
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
---
+++ src/Pokemon.js
@@ -0,0 +1,70 @@
+import React,{ useEffect, useState, useRef } from 'react'
+import axios from 'axios'
+
+const  PokemonList = () => {
+    const [data, setData] = useState()
+    const [offset, setOffset] = useState(0)
+    const limit = useRef(100)
+    useEffect(() => {
+        (async function getData() {
+            await axios
+                .get('https://pokeapi.co/api/v2/pokemon', {
+                    params: {
+                        limit: limit.current,
+                        offset,
+                    },
+                })
+                .then((res) => setData(res.data))
+                .catch((err) => {
+                    console.log(err)
+                    return []
+                })
+        })()
+    }, [offset])
+    return (
+        <div>
+            <div className="p-2">
+                <button
+                    disabled={!data?.previous}
+                    className="btn btn-primary"
+                    onClick={() => {
+                        if (!!data?.previous) setOffset((prev) => prev - limit.current)
+                    }}
+                >
+                    Presious
+                </button>
+                <button
+                    disabled={!data?.next}
+                    className="btn btn-outline-dark"
+                    onClick={() => {
+                        if (!!data?.next) setOffset((prev) => prev + limit.current)
+                    }}
+                >
+                    Next
+                </button>
+            </div>
+            <table className="table table-striped w-50">
+                <thead>
+                    <tr>
+                        <th>STT</th>
+                        <th>Name Pokemon</th>
+                        <th>Button</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data?.results.map((pokemon, index) => (
+                        <tr key={index}>
+                            <td>{index + 1}</td>
+                            <td>{pokemon.name}</td>
+                            <td>
+                                <a href={pokemon.url}>Detail</a>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default PokemonList;
